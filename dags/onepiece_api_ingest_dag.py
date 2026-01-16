1c884c4ab055fb35873656e9e8789554c04a32fc
David <david.develop.13@gmail.com>
2026-01-16 13:45:12 +0100

    UPD to work with connection

--- dags/onepiece_api_ingest_dag.py
+++ dags/onepiece_api_ingest_dag.py
@@ -1,23 +1,10 @@
-from airflow import DAG
-from airflow.providers.standard.operators.python import PythonOperator
-from airflow.sdk import Variable, chain
-from airflow.decorators import dag, task
+from airflow.sdk import Variable, dag, task
+from airflow.hooks.base import BaseHook
 from datetime import datetime
-from api_utils import fetch_data_from_api
+from pathlib import Path
+from api_utils import fetch_data_from_api, write_data_to_jsonl
 
 import logging
-
-API_CONFIG = Variable.get("API", deserialize_json=True)
-INGESTION_CONFIG = Variable.get("INGESTION", deserialize_json=True)
-
-API_URL = API_CONFIG["URL"]
-API_ENDPOINTS_DICT = API_CONFIG["ENDPOINTS"]                                 
-API_LANG = API_CONFIG["LANGUAGE"]
-
-INGESTION_TIMEOUT = INGESTION_CONFIG["TIMEOUT_SECONDS"]
-INGESTION_RETRIES = INGESTION_CONFIG["RETRIES"]
-
-ENDPOINTS = tuple(API_ENDPOINTS_DICT.keys())
 
 @dag(
     dag_id="onepiece_api_ingest_dag",
@@ -27,17 +14,54 @@ ENDPOINTS = tuple(API_ENDPOINTS_DICT.keys())
 )
 def run_dag():
 
+    API_CONFIG = Variable.get("API", deserialize_json=True)
+    INGESTION_CONFIG = Variable.get("INGESTION", deserialize_json=True)
+    API_CONN = BaseHook.get_connection(API_CONFIG["CONN_ID"])
+
+    API_URL = f"{API_CONN.schema}://{API_CONN.host}/v2"
+    API_ENDPOINTS_DICT = API_CONFIG["ENDPOINTS"]                                 
+    API_LANG = API_CONFIG["LANGUAGE"]
+
+    INGESTION_TIMEOUT = INGESTION_CONFIG["TIMEOUT_SECONDS"]
+    INGESTION_RETRIES = INGESTION_CONFIG["RETRIES"]
+
+    ENDPOINTS = tuple(API_ENDPOINTS_DICT.keys())
+
     @task()
-    def fetch_and_log_data(**kwargs):
-        endpoint = kwargs["endpoint"]
+    def fetch_and_log_data(endpoint: str, ds: str) -> None:
         api_endpoint = f"{API_URL}/{API_ENDPOINTS_DICT[endpoint]}/{API_LANG}"
-        data = fetch_data_from_api(api_endpoint)
-        logging.info(f"Fetched data for endpoint {endpoint}: {data}")
+        target_path = Path(f"/data/raw/{endpoint}/ingestion_date={ds}/data.jsonl")
+        
+        logging.info(
+            "Starting ingestion for endpoint=%s",
+            endpoint,
+        )
+
+        data = fetch_data_from_api(api_endpoint, timeout=INGESTION_TIMEOUT)
+
+        record_count = len(data)
+        #if record_count == 0:
+        #    raise ValueError(f"No records fetched for endpoint={endpoint}. Failing task.")
+        
+        logging.info(
+            "Fetched %d records for endpoint=%s",
+            record_count,
+            endpoint,
+        )
+
+        write_data_to_jsonl(data, target_path)
+
+        logging.info(
+            "Successfully wrote %d records to %s",
+            record_count,
+            target_path,
+        )
+
 
     list(
         fetch_and_log_data
             .override(task_id=f"fetch_{endpoint}_data")
-            (endpoint=endpoint)
+            (endpoint=endpoint, ds="{{ ds }}")
         for endpoint in ENDPOINTS
     )
 
