ac46c46a5ba92549561275f493b8b5d19b9d7db4
David <david.develop.13@gmail.com>
2026-05-24 22:36:13 +0200

    ADD some changes

--- dags/basic_test_dag.py
+++ dags/basic_test_dag.py
@@ -1,12 +1,19 @@
+from airflow.sdk import Variable
 from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.hooks.http import HttpHook
 from datetime import datetime
 
 import logging
 
-def say_hello():
-    logging.info("Hello from Airflow 3")
+def get_data():
+    hook = HttpHook(method='GET', http_conn_id='onepiece_api')
+    response = hook.run(endpoint='/v2/characters/en')
+    logging.info(f"API Response Status: {response.status_code}")
+    logging.info(f"API Response Content: {response.text}")
+    logging.info(f"API Response Type: {type(response.json())}")
 
 with DAG(
     dag_id="basic_test_dag",
@@ -16,9 +23,19 @@ with DAG(
     is_paused_upon_creation=False,
 ) as dag:
 
+    http_sensor = HttpSensor(
+        task_id="check_api",
+        http_conn_id="onepiece_api",
+        endpoint="/v2/characters/en",
+        poke_interval=10,
+        timeout=30,
+        response_check=lambda response: response.status_code == 200,
+        mode="poke",
+    )
+
     hello_from_python = PythonOperator(
-        task_id="say_hello",
-        python_callable=say_hello
+        task_id="get_api_data",
+        python_callable=get_data
     )
 
     hello_from_bash = BashOperator(
@@ -26,4 +43,4 @@ with DAG(
         bash_command='echo "Hello from Airflow Bash Operator"'
     )
 
-    hello_from_python >> hello_from_bash
+    http_sensor >> hello_from_python >> hello_from_bash
